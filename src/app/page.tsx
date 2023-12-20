f1f7dfc69db8aa97b6ab12afea2adc0070a7d3fe
ManishKumar <manishkumar97430@gmail.com>
2023-12-20 13:14:23 +0530

    added the route guard for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import UrlShortner from "@/components/providers/shortner-provider";
+import { getAuthSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    redirect("/auth");
+  }
+
   return <UrlShortner />;
 }
